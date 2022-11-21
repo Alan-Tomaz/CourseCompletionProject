697dae51de8ac4631fa5003722b08bc3186f8da3
Alan <90423094+Alan-Tomaz@users.noreply.github.com>
2022-11-20 23:58:12 -0300

    Add files via upload

---
+++ ProgramaDeGestão/src/dal/ModuloConexao.java
@@ -0,0 +1,33 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author alan4
+ */
+public class ModuloConexao {
+
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/rc_db";
+        String user = "root";
+        String password = "";
+        
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            System.out.println(e);
+           return null;
+        }
+    }
+
+}
